99c56b84a40e0df8fa63fa8cb8c9a49ecac69fdd
Taylor Lehman <taylorlehman111@gmail.com>
2016-11-29 23:28:25 -0800

    initial backbone

--- server-config.js
+++ server-config.js
@@ -1,7 +1,11 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
 
-
+console.log(__dirname);
+app.use(bodyParser.json());
+app.use(morgan('dev'));
 app.use(express.static(__dirname + '/client'));
 
 
