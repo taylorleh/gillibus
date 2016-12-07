d45a409ac1fe1293157c3665797d9d020da136ef
Taylor Lehman <taylorlehman111@gmail.com>
2016-12-06 20:59:12 -0800

    adds routes module and google maps directives

--- client/app.js
+++ client/app.js
@@ -2,31 +2,44 @@ angular.module('gillibus', [
   'gillibus.locations',
   'gillibus.pricing',
   'gillibus.home',
+  'gillibus.routes',
+  'uiGmapgoogle-maps',
   'ngRoute'
 
-]).config(function($routeProvider, $httpProvider) {
+]).config(function($routeProvider, uiGmapGoogleMapApiProvider) {
   $routeProvider
-    .when('/home', {
+    .when('/', {
       templateUrl: 'home/homeTemplate.html',
       controller: 'HomeController'
     })
+    .when('/routes', {
+      templateUrl: 'routes/routesTemplate.html',
+      controller: 'RoutesController'
+    })
     .when('/pricing', {
-      templateUrl:'pricing/pricingTemplate.html',
-      controller:'PricingController'
+      templateUrl: 'pricing/pricingTemplate.html',
+      controller: 'PricingController'
     })
     .when('/jose', {
-      templateUrl:'locations/locationsTemplate.html',
-      controller:'LocationsController'
+      templateUrl: 'locations/locationsTemplate.html',
+      controller: 'LocationsController'
     })
     .when('/jose', {
-      templateUrl:'locations/locationsTemplate.html',
-      controller:'LocationsController'
+      templateUrl: 'locations/locationsTemplate.html',
+      controller: 'LocationsController'
     })
     .when('/jose', {
-      templateUrl:'locations/locationsTemplate.html',
-      controller:'LocationsController'
+      templateUrl: 'locations/locationsTemplate.html',
+      controller: 'LocationsController'
     })
-    .otherwise('/home')
+    .otherwise('/');
+
+  //  configure google maps provider
+  uiGmapGoogleMapApiProvider.configure({
+    key: 'AIzaSyDs4rZOUR9C6GjW-FUifZ_kNZpnv9WjS6U&callback',
+    v: '3', //defaults to latest 3.X anyhow
+    libraries: 'geometry'
+  });
 
 }).run(function($rootScope, $location) {
   console.log('RUNNING')
