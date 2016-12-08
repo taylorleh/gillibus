3b0a8e70d73aeabf6657bdc7ba3fa0228c3d9479
Taylor Lehman <taylorlehman111@gmail.com>
2016-12-08 02:06:50 -0800

    booking and reservation scaffolding

--- client/charter/charterController.js
+++ client/charter/charterController.js
@@ -1,5 +1,44 @@
 angular.module('gillibus.charter', [])
-  .controller('CharterController', ['$scope', function($scope) {
+  .controller('CharterController', ['$scope','moment', function($scope, moment) {
+
+    // Calendar
+    $scope.calendar = {
+      open: false,
+      toggle:function() {
+        console.log('calendar toggle', this);
+        this.open = !this.open;
+      },
+      dayTiles: [],
+      populateTiles:function(base) {
+        let currentDate = base;
+        let nextDate = moment(base).add(1, 'days');
+        do {
+          this.dayTiles.push(currentDate.format("dddd, MMMM Do"));
+          currentDate = nextDate;
+          nextDate = moment(nextDate).add(1, 'days');
+        } while (this.dayTiles.length < 7);
+      }
+    };
+
+    // Carousel
+    $scope.slickConfig = {
+      enabled: true,
+      autoplay: false,
+      draggable: false,
+      slickPause:'',
+      autoplaySpeed: 3000,
+      slidesToShow: 5,
+      method: {},
+      event: {
+        beforeChange: function(event, slick, currentSlide, nextSlide) {
+        },
+        afterChange: function(event, slick, currentSlide, nextSlide) {
+        }
+      }
+    };
+
+    $scope.calendar.populateTiles(moment());
+
 
   }]);
 
