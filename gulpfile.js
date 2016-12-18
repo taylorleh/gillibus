54ec7fc6d983487dcac8858cfc0f3f6278974090
Taylor Lehman <taylorlehman111@gmail.com>
2016-12-18 05:05:43 -0800

    adds font awesome

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,27 @@ const nodemon = require('gulp-nodemon');
 
 
 
+const paths = {
+  BOWER:"client/lib",
+  FONTS: "./client/assets/fonts",
+  FONT_AWESOME_OUT: "./client/less/font-awesome"
+};
+
+
+
+
+
+// gulp.task('fonts', function() {
+//   gulp.src(fontAwesome.fonts)
+//     .pipe(gulp.dest(paths.FONTS));
+// });
+
+
+gulp.task('fonts', function() {
+  return gulp.src(paths.BOWER + '/font-awesome/**/*.*')
+    .pipe(gulp.dest(paths.FONT_AWESOME_OUT));
+});
+
 
 gulp.task('less', function() {
   return gulp.src('./client/less/**/*.less')
