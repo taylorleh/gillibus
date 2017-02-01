5e969d3b551d5b99d503afcb437ab96279efae7f
Taylor Lehman <taylorlehman111@gmail.com>
2017-02-01 02:24:04 -0800

    countdown partial and countdown directive binding

--- client/directives/countdownSubheader.js
+++ client/directives/countdownSubheader.js
@@ -2,8 +2,15 @@ angular.module('gillibus.directive.countdownSubheader', [])
   .directive('countdown', [function () {
     return {
       restrict:'A',
-      link:function ($scope, element, attr) {
+      templateUrl:'../directives/countdownTemplate.html',
+      scope: {
+        countdown: '='
+      },
+      link:function (scope, element, attrs) {
         console.log('countdown', arguments);
+        scope.$watch('countdown', function(newVal) {
+          console.log('timer changed calue', newVal);
+        })
       }
     }
   }]);
