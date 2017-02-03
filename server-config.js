e4933444256ca96be494d6400d94d3309e06e9bb
Taylor Lehman <taylorlehman111@gmail.com>
2017-02-03 02:03:33 -0800

    adds heroku ssl redirect plugin

--- server-config.js
+++ server-config.js
@@ -1,9 +1,8 @@
+var sslRedirect = require('heroku-ssl-redirect');
 var express = require('express');
 var app = express();
 
-
+app.use(sslRedirect());
 app.use(express.static(__dirname + '/client'));
-
-
 
 module.exports = app;
