3fb953d5dd3fe5cda352efd6fefde1c65e61eb6c
Taylor Lehman <taylorlehman111@gmail.com>
2017-03-04 09:40:25 -0800

    setup frontend payment process

--- client/directives/cardValidation.js
+++ client/directives/cardValidation.js
@@ -6,22 +6,44 @@ import angular from 'npm/angular';
 let moduleName = 'gillibus.directive.cardValidation';
 
 
-class StripeForm {
-  constructor() {
-    this.restrict = 'EA';
-    this.templateUrl = '../directives/partials/stripeForm.tpl.html';
-    this.replace = true;
-    this.scope = {
-      cardValidation: '=',
-      cardObject: '='
-    };
+function StripeForm(viewportService, $timeout) {
+  console.log('vp service', viewportService);
+  console.log('this', this);
 
+  // function onCardChange(result) {
+  //   console.log('on card change', result);
+  //   console.log('this', this);
+  // }
+
+  function init() {
+    let desktop = viewportService.isDesktop();
+    let style = {
+      base: {
+        color: '#31325F',
+        fontFamily: 'Helvetica Neue',
+        fontSize: desktop ? '22px' : '1.05rem',
+        fontWeight: 300,
+        iconColor: '#666EE8',
+        lineHeight: '65px',
+
+      },
+      invalid: {
+        color: '#cc4f55'
+      }
+
+    };
+    let elements = stripe.elements();
+    let card = elements.create('card', { style: style });
+    this.card = card;
+    card.on('change', this.onCardChange.bind(this));
+
+    $timeout(e => {
+      card.mount('#card-element');
+    });
   }
 
-  link(scope, element, attrs) {
-    let el = element;
-    let s = scope;
-    let self = this;
+  function _link(scope, element, attrs) {
+    scope.validation = null;
 
     scope.onCardChange = (result) => {
       console.log('result', result);
@@ -30,81 +52,41 @@ class StripeForm {
 
       if (result.token) {
 
-      } else if(result.error) {
+      } else if (result.error) {
         erEl.text(result.error.message);
         erEl.show();
       }
-
+      scope.validation = result;
     };
 
-    scope.bindCardChange = (cardObj) => {
-      if (cardObj) {
-        cardObj.on('change', angular.bind(scope, scope.onCardChange));
+    scope.processPayment = (event) => {
+      if (scope.validation && scope.validation.complete) {
+        console.log('IS VALID SUBMITTING!');
+        stripe.createToken(scope.card)
+          .then(result => {
+            console.log('GOT RESPONSE', result);
+          })
       }
     };
 
-    scope.$watch('cardObject', angular.bind(scope, scope.bindCardChange));
 
-    scope.$watch('cardValidation', e => {
-
-    })
+    init.call(scope);
   }
 
 
-  static stripeFormFactory() {
-    StripeForm.instance = new StripeForm();
-  }
+  return {
+    restrict: 'EA',
+    templateUrl: '../directives/partials/stripeForm.tpl.html',
+    replace: true,
+    scope: {
+      cardValidation: '=',
+      cardObject: '='
+    },
+    link: _link
+  };
 }
 
+StripeForm.$inject = ['viewportService', '$timeout'];
 
-
-
-//
-// function StripeForm() {
-//   function _link(scope, element, attrs) {
-//       let el = element;
-//       let s = scope;
-//       let self = this;
-//
-//       scope.onCardChange = (result) => {
-//         console.log('result', result);
-//         element.find('.success').hide();
-//         let erEl = element.find('.error').hide();
-//
-//         if (result.token) {
-//
-//         } else if(result.error) {
-//           erEl.text(result.error.message);
-//           erEl.show();
-//         }
-//
-//       };
-//
-//       scope.bindCardChange = (cardObj) => {
-//         if (cardObj) {
-//           cardObj.on('change', angular.bind(scope, scope.onCardChange));
-//         }
-//       };
-//
-//       scope.$watch('cardObject', angular.bind(scope, scope.bindCardChange));
-//
-//       scope.$watch('cardValidation', e => {
-//
-//       })
-//   }
-//
-//
-//   return {
-//     restrict: 'EA',
-//     templateUrl: '../directives/partials/stripeForm.tpl.html',
-//     replace: true,
-//     scope: {
-//       cardValidation: '=',
-//       cardObject: '='
-//     },
-//     link: _link
-//   };
-// }
-
-angular.module(moduleName, []).directive('stripeForm', StripeForm.stripeFormFactory);
+angular.module(moduleName, []).directive('stripeForm', StripeForm);
 export default moduleName;
