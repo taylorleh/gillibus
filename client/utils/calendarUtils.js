ab39d6df59ef6f6dc1c14b08b8976e7e49543e0c
Taylor Lehman <taylorlehman111@gmail.com>
2017-03-17 03:52:08 -0700

    overhaul in utils, now adds disable flag if all buses are booked for given block

--- client/utils/calendarUtils.js
+++ client/utils/calendarUtils.js
@@ -4,6 +4,25 @@
 import moment from 'moment';
 let calendarUtils = {};
 
+let disableBlocksWhenAllBusesBlocked = (checkDates, datesHash, busNames) => {
+  checkDates.forEach(date => {
+    let {dateScope, dayCount, nightCount} = {dateScope: datesHash[date], dayCount: 0, nightCount: 0};
+    busNames.forEach(bus => {
+      !dateScope[bus].day && (dayCount += 1);
+      !dateScope[bus].night && (nightCount += 1);
+    });
+
+    if(dayCount === busNames.length) {
+      dateScope.day = false;
+    }
+
+    if(nightCount === busNames.length) {
+      dateScope.night = false;
+    }
+  })
+
+};
+
 
 /**
  * Returns if an event is booked for either day or night
@@ -14,7 +33,7 @@ let calendarUtils = {};
  *
  */
 calendarUtils.bookedNightOrDay = (start, end) => {
-  if(start.hour() > 14) {
+  if (start.hour() > 14) {
     return 'night';
   }
   return 'day';
@@ -23,16 +42,16 @@ calendarUtils.bookedNightOrDay = (start, end) => {
 calendarUtils.daysOfMonthHash = (busIds) => {
 
   let hash = Array.apply(null, Array(42)).reduce((memo, _, ind) => {
-    let targetDate = moment().startOf('day').date(ind+1).toDate();
+    let targetDate = moment().startOf('day').date(ind + 1).toDate();
     let target = memo[targetDate];
-    memo[targetDate] = {day: null, night: null};
+    memo[targetDate] = { day: true, night: true };
     return memo;
   }, {});
 
   Object.keys(hash).forEach(d => {
     let ctx = hash[d];
     busIds.forEach(id => {
-      ctx[id] = {day: true, night: true};
+      ctx[id] = { day: true, night: true };
     });
   });
   return hash;
@@ -49,18 +68,33 @@ calendarUtils.daysOfMonthHash = (busIds) => {
 // timeZone:"America/Los_Angeles"
 // updated:"2017-03-16T05:03:56.037Z"
 
-calendarUtils.applyCalendarEventsToUnified = (eventData, scopeObj) => {
-  let busName = /(?:-)(\w*)(?:-)/i.exec(eventData.summary)[1].toUpperCase();
-  eventData.items.forEach(gcalEvent => {
-    let start, end;
-    ({start, end} = {start: moment(gcalEvent.start.dateTime), end: moment(gcalEvent.end.dateTime)});
+/**
+ *
+ * @param {Object[]} busesEvents - array of event data for each bus
+ * @param {Object} scopeObj - date hash
+ * @param {String[]} busNames - array of bus name accessors
+ * @param  moment - moment object
+ * @return {*}
+ */
+calendarUtils.applyCalendarEventsToUnified = (busesEvents, scopeObj, busNames,  moment) => {
+  let eventsOnDates = [];
+  busesEvents.forEach((busEvents, index) => {
+    busEvents.forEach(event => {
+      let {start, end} = { start: moment(event.start.dateTime), end: moment(event.end.dateTime) };
+      let { eventTime, dateScope } = {
+        eventTime: calendarUtils.bookedNightOrDay(start, end),
+        dateScope: scopeObj[start.startOf('day').toDate()]
+      };
 
-    let eventTime = calendarUtils.bookedNightOrDay(start, end);
-    let dateScope = scopeObj[start.startOf('day').toDate()];
-    dateScope[busName] = dateScope[busName] || {};
-    dateScope[busName][eventTime] = false;
+      dateScope[busNames[index]][eventTime] = false;
 
+      if (eventsOnDates.indexOf(start.toDate()) === -1) {
+        eventsOnDates.push(start.toDate());
+      }
+    });
   });
+
+  disableBlocksWhenAllBusesBlocked(eventsOnDates, scopeObj, busNames);
 
   return scopeObj;
 };
