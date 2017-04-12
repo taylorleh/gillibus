6884dc0e6610151032e78eb70a833737b76ae80d
Taylor Lehman <taylorlehman111@gmail.com>
2017-04-11 20:41:45 -0700

    fixes user model check password instance method

--- server/models/user.js
+++ server/models/user.js
@@ -2,54 +2,10 @@
  * Created by taylor on 3/22/17.
  */
 
+"use strict";
 
 const bcrypt = require('bcrypt-nodejs');
 const _ = require('lodash');
-
-
-//
-// let ModelBase = require('bookshelf-modelbase')(require('../db/config'));
-// let Promise  = require('bluebird');
-// let bcrypt = Promise.promisifyAll(require('bcrypt-nodejs'));
-//
-// let User = ModelBase.extend({
-//   tableName: 'tbl_admin_users',
-//
-//   virtuals: {
-//     password: {
-//       get: function() {
-//         return this.pass;
-//       },
-//       set: function(value) {
-//         this.pass = value;
-//       }
-//     }
-//
-//   },
-//
-//   outputVirtuals: false,
-//
-//   initialize: function() {
-//     this.on('saving', this._hashUser.bind(this));
-//
-//   },
-//
-//   verifyPassword: function(password, hash, done) {
-//     bcrypt.compare(password, hash , function(err, result) {
-//       return done(err, result);
-//     });
-//   },
-//
-//   _hashUser: function() {
-//     let salt = bcrypt.genSaltSync();
-//     let hash = bcrypt.hashSync(this.get('password'), salt);
-//     this.set('hash', hash);
-//     this.set('salt', salt);
-//   }
-// });
-
-
-"use strict";
 
 module.exports = function(sequelize, DataTypes) {
   return sequelize.define("Users", {
@@ -81,12 +37,16 @@ module.exports = function(sequelize, DataTypes) {
       },
       checkPassword: function(pass) {
         let storedHash = this.get('hash');
+        let instance = this;
         return new Promise((resolve, reject) => {
           bcrypt.compare(pass, storedHash, function(err, result) {
-            if(err) {
-              reject(err);
+            if(err || !result) {
+              reject({ message: 'Incorrect login credentials'});
             } else {
-              resolve(result);
+              resolve({
+                result: result,
+                instance: instance
+              });
             }
           })
         });
