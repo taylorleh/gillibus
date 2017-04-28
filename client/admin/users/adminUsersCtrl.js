b92ba8b81c26fe845155bf8e4b7f63ee7f38fe8e
Taylor Lehman <taylorlehman111@gmail.com>
2017-04-27 22:47:58 -0700

    refine users admin page with modal

--- client/admin/users/adminUsersCtrl.js
+++ client/admin/users/adminUsersCtrl.js
@@ -5,33 +5,53 @@
 let moduleName = 'gillibus.admin.users';
 class AdminUsers {
 
-  constructor($scope, $state, adminPortalService) {
-    angular.element('.front-nav').hide();
+  constructor($scope, $state, adminPortalService, $uibModal) {
     this.adminPortalService = adminPortalService;
-
+    this.$uibModal = $uibModal;
+    this.$scope = $scope;
     this.users = [];
-
     this.newEmail = '';
 
     adminPortalService.getAdminUsers()
       .then(this.adminUsersHandler.bind(this));
-
   }
 
-//  --------------- RESPONSE HANDLERS ----------------
+  //  --------------- RESPONSE HANDLERS ----------------
 
   adminUsersHandler(response) {
     this.users = response.data.users;
   }
 
+  // --------------- ACTIONS --------------------------
+
   addNewAdminUser(email) {
+    this.modalInstance.close();
     this.adminPortalService.requestNewUserToken(email);
   }
 
+  cancel() {
+    this.modalInstance.close();
+  }
+
+
+  openNewUserModal() {
+
+    let modalInstance = this.$uibModal.open({
+      templateUrl: 'admin/partials/newUserPtl.html',
+      size: 'sm',
+      scope: this.$scope
+    });
+
+    this.modalInstance = modalInstance;
+
+    modalInstance.result.then(function(results) {
+    });
+
+  }
 
 
 }
-AdminUsers.$inject = ['$scope', '$state', 'adminPortalService'];
+AdminUsers.$inject = ['$scope', '$state', 'adminPortalService', '$uibModal'];
 
 angular.module(moduleName, []).controller('AdminUsersCtrl', AdminUsers);
 export default moduleName
