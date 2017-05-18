229281f8edd8dc47d89c852d6adc9469124c535a
Taylor Lehman <taylorlehman111@gmail.com>
2017-05-18 00:31:27 -0700

    WIP - add vue files

---
+++ client/vue-admin/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <hello-component></hello-component>
+</template>
+<style>
+
+</style>
+<script>
+  import HelloComponent from './components/HelloComponent.vue';
+  export default {
+    components: { HelloComponent }
+  }
+</script>
---
+++ client/vue-admin/src/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by taylor on 5/16/17.
+ */
+import Vue from 'vue';
+import App from './App.vue';
+
+new Vue({
+  el: 'body',
+  components: { App },
+  render: (h) => h(App)
+})
