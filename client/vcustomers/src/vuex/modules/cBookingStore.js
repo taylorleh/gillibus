53de78f9bd353397afe58a9f0b6e86504b9b4318
Taylor Lehman <taylorlehman111@gmail.com>
2017-06-07 03:04:31 -0700

    modify bookingStore to use new axios api

--- client/vcustomers/src/vuex/modules/cBookingStore.js
+++ client/vcustomers/src/vuex/modules/cBookingStore.js
@@ -3,11 +3,15 @@
  */
 import Vue from 'vue';
 import moment from 'moment';
+import api from '../../api/calendarResource';
+
+
+const busKeys = ['CHARTER_G3', 'CHARTER_CHARLIE', 'CHARTER_STARSHIP', 'CHARTER_GILLIBUS'];
 
 const state = {
   events: [],
   monthKey: (new Date()).getMonth(),
-  yearKey:(new Date()).getUTCFullYear()
+  yearKey: (new Date()).getUTCFullYear()
 };
 
 const mutations = {
@@ -23,30 +27,47 @@ const mutations = {
 
 };
 
+//
+// fetchEvents({ commit }, payload) {
+//   Vue.http.post('calendar/events', {
+//       calendar: payload.calendar,
+//       timeMin: payload.timeMin
+//     })
+//     .then(response => {
+//       return response.json();
+//     })
+//     .then(data => {
+//       console.log('new items', data);
+//       let allItems = data.items.map(item => {
+//         return {
+//           title: item.description,
+//           start: item.start.dateTime,
+//           end: item.end.dateTime
+//         }
+//       });
+//       commit('SET_EVENTS', allItems);
+//     })
+
 const actions = {
   fetchEvents({ commit }, payload) {
-    Vue.http.post('calendar/events', {
-        calendar: payload.calendar,
-        timeMin: payload.timeMin
-      })
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        console.log('new items', data);
-        let allItems = data.items.map(item => {
-          return {
-            title: item.description,
-            start: item.start.dateTime,
-            end: item.end.dateTime
-          }
-        });
-        commit('SET_EVENTS', allItems);
-      })
+    let { timeMin, timeMax } = payload;
+    busKeys.forEach(key => {
+      api.post('/events', {
+          timeMin,
+          timeMax,
+          calendar: key
+        })
+        .then(res => {
+          // console.log('got cal events', res);
+        })
+        .catch(error => {
+          console.error('failed', error);
+        })
+    })
   },
   SUBTRACT_MONTH({ commit, state }) {
-    if(state.monthKey -1 >= 0) {
-      commit('SET_MONTH', state.monthKey-1);
+    if (state.monthKey - 1 >= 0) {
+      commit('SET_MONTH', state.monthKey - 1);
     } else {
       let year = moment().year(state.yearKey).subtract(1, 'year').year();
       commit('SET_YEAR', year);
@@ -54,8 +75,8 @@ const actions = {
     }
   },
   ADD_MONTH({ commit }) {
-    if(state.monthKey+1 <= 11) {
-      commit('SET_MONTH', state.monthKey+1);
+    if (state.monthKey + 1 <= 11) {
+      commit('SET_MONTH', state.monthKey + 1);
     } else {
       let year = moment().year(state.yearKey).add(1, 'year').year();
       commit('SET_YEAR', year);
