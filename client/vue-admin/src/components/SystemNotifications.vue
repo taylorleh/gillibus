8a40f86770e29b482193ff32a71344eab1e922a1
Taylor Lehman <taylorlehman111@gmail.com>
2017-06-12 14:50:08 -0700

    adds systems notifications vue file

---
+++ client/vue-admin/src/components/SystemNotifications.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container admin-notifications">
+    <div class="row">
+      <div class="col-xs-10 col-xs-offset-1">
+        <el-alert
+          v-for="message in successes"
+          :title="message.title"
+          :key="message"
+          type="success"
+          :description="message.description"
+          @close="close(message)"
+          show-icon>
+        </el-alert>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import { mapGetters, mapActions } from 'vuex';
+
+  export default {
+    name: 'SystemNotifications',
+
+    computed: {
+      ...mapGetters({
+        successes: 'successes',
+        getMessage: 'message'
+      })
+    },
+
+
+    methods: {
+      ...mapActions({
+        remove: 'removeMessage'
+      }),
+
+      close(message) {
+        this.remove(message);
+      }
+
+    },
+
+
+    created() {
+
+    }
+  }
+</script>
+<style lang="less">
+  .container.admin-notifications {
+
+    margin-bottom: 1em;
+  }
+
+</style>
