e8832b4763604e5bf38feca2ee1fdecab21b0ca6
Taylor Lehman <taylorlehman111@gmail.com>
2017-06-12 14:50:48 -0700

    checkout utils

---
+++ client/vue-admin/src/util/checkoutUtils.js
@@ -0,0 +1,14 @@
+/**
+ * Created by taylor on 6/10/17.
+ */
+
+export const pluralTypes = {
+  success: 'successes',
+  warning: 'warnings',
+  error: 'errors'
+};
+
+
+export const TYPE_MUTATIONS = {
+
+}
