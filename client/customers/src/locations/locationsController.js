37cd3bec0246bcf445d62eb48e54caed22e8cc08
Taylor Lehman <taylorlehman111@gmail.com>
2017-07-13 19:47:39 -0700

    removes angular dir

--- client/customers/src/locations/locationsController.js
+++
@@ -1,6 +0,0 @@
-import angular from 'npm/angular';
-export default angular.module('gillibus.locations', [])
-  .controller('LocationsController',
-    ['$scope', function($scope) {
-    console.log('LOADED Locations CONTROLLER');
-  }]);
--- client/customers/src/pricing/pricingController.js
+++
@@ -1,5 +0,0 @@
-import angular from 'npm/angular';
-export default angular.module('gillibus.pricing', [])
-  .controller('PricingController', ['$scope', function($scope) {
-    console.log('LOADED Pricing CONTROLLER');
-  }]);
